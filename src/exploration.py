df2d16a496c71d3f354c91bb7dd773d0ed0a7af9
JuanPabloDP <juanpablo.dp.glez@gmail.com>
2025-10-26 18:53:05 -0600

    commit - exploration partially complete, already running

--- src/exploration.py
+++ src/exploration.py
@@ -2,18 +2,18 @@ import numpy as np
 
 from generate import generate
 
-def exploration(X,max_R,p_end,p_back):
+def exploration(x,max_R,p_end,p_back):
     class son:
-        def _init_(self, individual, parent):
+        def __init__(self, individual, parent):
             self.individual = individual 
             self.parent = parent
-    X=son(X, np.nan)
+    X=son(individual=x, parent=np.nan)
     new_sols=[X.individual]
     X_init=X.individual
-    R=np.random.uniform(max_R)
+    R=np.random.uniform(0,max_R)
     path=[X.individual]
     while True:
-        X_1=son(generate(X.individual,R), X)
+        X_1=son(individual=generate(X.individual,R), parent=X)
         new_sols.append(X_1.individual)
         path.append(X_1.individual)
         X=X_1
@@ -24,4 +24,7 @@ def exploration(X,max_R,p_end,p_back):
             break
         #Evaluate
         #X_best=X_best.indivual (np array)
-    return 
+    return new_sols,path
+
+x=np.array([3.5,3.0])
+sols,path=exploration(x,1.0,0.2,0.2)
--- src/generate.py
+++ src/generate.py
@@ -1,16 +1,16 @@
 import numpy as np
 import math as mt
 
-def generate(X,r):
-    v=np.zeros(len(X))
+def generate(x,r):
+    v=np.zeros(len(x))
     k=0
-    for item in v:
+    for i in range(len(v)):
         prob=1/mt.factorial(k)
         u=np.random.rand()
         if u<=prob:
-            item=1
+            v[i]=1
             k+=1
-    V=np.random.permutation()
-    U=np.random.rand(len(X))
-    X_1=X+V*(U*2*r-r)
-    return X_1
+    V=np.random.permutation(v)
+    U=np.random.rand(len(x))
+    x_1=x+V*(U*2*r-r)
+    return x_1
